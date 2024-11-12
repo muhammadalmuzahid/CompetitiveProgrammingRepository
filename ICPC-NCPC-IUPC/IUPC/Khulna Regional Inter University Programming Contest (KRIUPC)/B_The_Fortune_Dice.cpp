4add22e9d4db96416defafc5e934f05be3523759
MUHAMMAD AL-MUZAHID <almuzahid16@cse.pstu.ac.bd>
2024-11-12 19:28:40 +0600

    Create B_The_Fortune_Dice.cpp

---
+++ ICPC-NCPC-IUPC/IUPC/Khulna Regional Inter University Programming Contest (KRIUPC)/B_The_Fortune_Dice.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define debug(x) cout << #x << " = "; cout << x << '\n';
+using namespace std;
+typedef long long ll;
+const int ARRAY_SIZE  = (int) 2e6+3;
+
+void idea() {
+    int x;
+    cin >> x;
+    cout << ((x >= 2 and x <= 12) ? "Yes" : "No") << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int T = 1;
+    // cin >> T;
+    for(int C = 1; C <= T; C++) {
+        // cout << "Case " << C << ": " << '\n';
+        idea();
+    }
+    return 0;
+}
