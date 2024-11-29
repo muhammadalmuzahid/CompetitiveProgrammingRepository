62b7603bee2072765b8e5d61be2eca3acd953a37
MUHAMMAD AL-MUZAHID <almuzahid16@cse.pstu.ac.bd>
2024-11-30 00:07:44 +0600

    Update Bitwise Operation.cpp

--- Ultimate Topic List/Chapter-2 Bit Manipulation/2.1 Bitwise Operation/Bitwise Operation.cpp
+++ Ultimate Topic List/Chapter-2 Bit Manipulation/2.1 Bitwise Operation/Bitwise Operation.cpp
@@ -1,21 +1,43 @@
 #include <bits/stdc++.h>
-#define debug(x) cout << #x << " = "; cout << x << '\n';
 using namespace std;
-typedef long long ll;
-const int ARRAY_SIZE  = (int) 2e6+3;
 
+int check_kth_bit_on_or_off(int x, int k) {
+    return (x >> k) & 1;
+}
 
-void idea() {
-    
+void print_on_and_off_bits(int x) {
+    for (int k = 0;k <= 31;k++) {
+        if (check_kth_bit_on_or_off(x, k)) {
+            cout << 1 << " ";
+        }
+        else {
+            cout << 0 << " ";
+        }
+    }
+    cout << '\n';
+}
+
+int turn_on_kth_bit(int x, int k) {
+    return (x | (1 << k));
+}
+
+int turn_off_kth_bit(int x, int k) {
+    return (x & (~(1 << k)));
+}
+
+int toggle_kth_bit(int x, int k) {
+    return (x ^ (1 << k));
 }
 
 int main() {
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    int T = 1;
-    // cin >> T;
-    for(int C = 1; C <= T; C++) {
-        // cout << "Case " << C << ": " << '\n';
-        idea();
-    }
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // cout << check_kth_bit_on_or_off(44, 3) << '\n';
+    // cout << check_kth_bit_on_or_off(44, 4) << '\n';
+    // print_on_and_off_bits(44);
+    // cout << turn_on_kth_bit(44, 4);
+    // cout << turn_off_kth_bit(44, 3);
+    cout << toggle_kth_bit(44, 3) << '\n';
     return 0;
 }
