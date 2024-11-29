5db93a3dbfacaf1855953d50fbca18e0f4b53a20
MUHAMMAD AL-MUZAHID <almuzahid16@cse.pstu.ac.bd>
2024-11-30 02:49:58 +0600

    Update sliding_window_median.cpp

--- Ultimate Topic List/Chapter-5 Data Structures/5.1 PBDS/sliding_window_median.cpp
+++ Ultimate Topic List/Chapter-5 Data Structures/5.1 PBDS/sliding_window_median.cpp
@@ -5,38 +5,29 @@
 using namespace __gnu_pbds;
 using namespace std;
 
-// Define a policy-based data structure (pbds) with order statistics
 template <typename T>
 using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+    ios::sync_with_stdio(false); cin.tie(NULL);
 
     int n, k;
     cin >> n >> k;
     vector<int> a(n);
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
+    for (int &x : a) cin >> x;
 
-    int l = 0, r = 0;
+    int l = 0;
     pbds<pair<int, int>> p;
-    while (r < n) {
+    for (int r = 0; r < n; r++) {
         p.insert({a[r], r});
         if (r - l + 1 == k) {
-            int pos = k / 2;
-            if (k % 2 == 0) {
-                pos--;
-            }
+            int pos = (k - 1) / 2;
             auto it = p.find_by_order(pos);
             cout << it->first << " ";
             p.erase({a[l], l});
             l++;
         }
-        r++;
     }
     cout << '\n';
-
     return 0;
 }
